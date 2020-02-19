3451de9b00b4640e3520679ca84de7ed1c47f771
Guillaume Blaquiere <guillaume.blaquiere@veolia.com>
2020-02-19 14:17:04 +0100

    Go pretty print connexion string

--- go/function/function.go
+++ go/function/function.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	_ "gopkg.in/goracle.v2"
 	"net/http"
+	"os"
 )
 
 func OracleConnection(w http.ResponseWriter, r *http.Request) {
 
 	dbIp := os.Getenv("ORACLE_IP")
 	dbSchema := os.Getenv("ORACLE_SCHEMA")
-    dbUser := os.Getenv("ORACLE_USER")
+	dbUser := os.Getenv("ORACLE_USER")
 	dbPassword := os.Getenv("ORACLE_PASSWORD")
 
-	db, err := sql.Open("goracle", dbUser + "/" + dbPassword + "@" + dbIp + ":1521/" + dbSchema)
+	db, err := sql.Open("goracle", fmt.Sprintf("%s/%s@%s:1521/%s", dbUser, dbPassword, dbIp, dbSchema))
 	if err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
